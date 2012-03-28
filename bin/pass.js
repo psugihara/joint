fc1c2a835d7f6292d438b04c95303030a018e54d
Peter Sugihara <peter@campsh.com>
2012-03-28 19:03:44 -0400

    started pass.js main entry script

---
+++ bin/pass.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+// Peter Sugihara
+// March 2012
+
+var sourcePath, port, staticPath;
+
+fetchArgs = function() {
+  var USAGE = 'Usage: pass program.pass [port# static directory]';
+  // print process.argv
+  if (process.argv.length != 3 && process.argv.length != 5) {
+    console.log(USAGE);
+    process.exit();
+  }
+
+  sourcePath = process.argv[2];
+
+  if (process.argv.length == 5) {
+    port = parseFloat(process.argv[3]);
+    if (port == Nan)
+      console.log(USAGE);
+    staticPath = process.argv[4];
+  }
+}
+
+fetchArgs();
