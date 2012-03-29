fd4d09e13d1032e49b9156f8106fd5d0d0ec771c
Peter Sugihara <peter@campsh.com>
2012-03-29 17:52:58 -0400

    Commented validation docco style.

--- bin/pass.js
+++ bin/pass.js
@@ -8,6 +8,9 @@ var fs = require('fs')
 
 var sourcePath, port, staticPath
 
+// Argument Validation
+// ___________________
+
 // Print a diagnostic message followed by the usage line, then die.
 var usageDie = function(message) {
   if (message)
@@ -16,22 +19,26 @@ var usageDie = function(message) {
   process.exit()
 }
 
+// Verify that the command was called with the correct number of arguments.
 if (process.argv.length !== 3 && process.argv.length !== 5) {
   usageDie('invalid number of arguments')
 }
 
 sourcePath = path.resolve(process.argv[2])
+// Verify that the source path exists and points to a Pass program.
 if (!path.existsSync(sourcePath))
   usageDie('file not found: ' + sourcePath)
 if (!path.extname(sourcePath) === 'pass')
   usageDie('Pass programs must have .pass extension')
 
 if (process.argv.length === 5) {
+  // Verify that the port is a number in the correct range.
   port = parseFloat(process.argv[3])
   if (!port || port < 0 || port > 65535) {
     usageDie('invalid port number')
   }
 
+  // Verify that the static path exists and points to a directory.
   staticPath = path.resolve(process.argv[4])
   if (!path.existsSync(staticPath))
     usageDie('file not found: ' + staticPath)
