178f17960fdbe73e95f5ddc5065f6a9f4daa6586
root <root@domU-12-31-39-09-59-B0.compute-1.internal>
2012-04-02 07:56:19 +0000

    added console-based chat

--- bin/pass.js
+++ bin/pass.js
@@ -45,10 +45,6 @@ if (process.argv.length === 5) {
   if (!fs.statSync(staticPath).isDirectory())
     usageDie('static arg must be directory')
 }
-else {
- 
-}
-
 
 var fs = require('fs')
 function handler (req, res) {
@@ -67,9 +63,14 @@ function handler (req, res) {
     fs.readFile(__dirname + req.url, response)
 }
 
+var clients = {};
+var callbacks = {};
+
 var app = require('http').createServer(handler).listen(port)
 
 var dnode = require('dnode')
-var server = dnode({
-  testTimesTwenty : function (n, cb) { cb(n * 20) }
-}).listen(app);
+var server = dnode(function (client, conn) {
+  this.register = function (name, cb) {conn.name = name; callbacks[conn.id] = cb; console.log(name + ' joined'); }
+  this.chat = function (message) { for(var i in callbacks) callbacks[i](conn.name, message); }
+
+}).listen(app)
