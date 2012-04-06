27c676a1b989e9e0f30113f08f739e3bcc161d0c
Andy Lamping <andrewlamping@gmail.com>
2012-04-06 00:07:46 -0400

    added Test.java and exp_test

---
+++ Tests/Test.java
@@ -0,0 +1,16 @@
+import org.antlr.runtime.*;
+
+public class Test {
+public static void main(String[] args) throws Exception {
+// Create an input character stream from standard in
+ANTLRInputStream input = new ANTLRInputStream(System.in);
+// Create an ExprLexer that feeds from that stream
+PassLexer lexer = new PassLexer(input);
+// Create a stream of tokens fed by the lexer
+CommonTokenStream tokens = new CommonTokenStream(lexer);
+// Create a parser that feeds off the token stream
+PassParser parser = new PassParser(tokens);
+// Begin parsing at rule prog
+parser.prog();
+}
+}
