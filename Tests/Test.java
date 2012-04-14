30bb0a9944408974ab063faaee890e3fb967f4ce
Andy Lamping <andrewlamping@gmail.com>
2012-04-13 23:43:57 -0400

    Added snippet_tests to test files that contains lines of code. Testing programs in the works

--- Tests/Test.java
+++ Tests/Test.java
@@ -4,7 +4,7 @@ public class Test {
 public static void main(String[] args) throws Exception {
 // Create an input character stream from standard in
 ANTLRInputStream input = new ANTLRInputStream(System.in);
-// Create an ExprLexer that feeds from that stream
+// Create a PassLexer that feeds from that stream
 PassLexer lexer = new PassLexer(input);
 // Create a stream of tokens fed by the lexer
 CommonTokenStream tokens = new CommonTokenStream(lexer);
