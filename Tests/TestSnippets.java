df8546e474649ddd0781e2b67c25f3d00ca9d1fe
Andy Lamping <andrewlamping@gmail.com>
2012-04-13 23:46:02 -0400

    Added TestSnippets.java

---
+++ Tests/TestSnippets.java
@@ -0,0 +1,24 @@
+import org.antlr.runtime.*;
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class TestSnippets {
+public static void main(String[] args) throws Exception {
+System.out.println(args[0]);
+DataInputStream is = new DataInputStream(new FileInputStream(args[0]));
+// Create an input character stream from standard in
+ANTLRInputStream input = new ANTLRInputStream(is);
+// Create an ExprLexer that feeds from that stream
+PassLexer lexer = new PassLexer(input);
+// Create a stream of tokens fed by the lexer
+CommonTokenStream tokens = new CommonTokenStream(lexer);
+// Create a parser that feeds off the token stream
+PassParser parser = new PassParser(tokens);
+// Begin parsing at rule prog
+parser.prog();
+}
+}
