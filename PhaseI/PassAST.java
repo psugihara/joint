96ad7e8d8109b0136e6c5f9f9af1e8163694983c
Cody De La Vara <cgd2120@columbia.edu>
2012-04-23 02:13:24 -0400

    Added custom node class to PassAST, and added an example of how our switch for the treewalker will work in the function walktree

--- PhaseI/PassAST.java
+++ PhaseI/PassAST.java
@@ -3,13 +3,27 @@ import org.antlr.runtime.Token;
 
 public class PassAST extends CommonTree {
 	public String text; 
+        public int type;
 	
-	public PassAST(Token t) {
+    public PassAST(Token t) {
 		super(t);
 		text = (t != null? t.getText(): "[]");
+		this.type = type;
 	}
 
 	public String toString() {
-		return text + super.toString();
+	    return text + " " + super.toString() + " " + super.getType();
 	}
+
+       
+    
+    public int get_type() {
+	return type;
+    }
+}
+
+class PassAdaptor extends CommonTreeAdaptor {
+    public Object create(Token payload) {
+	return new PassAST(payload);
+    }
 }
