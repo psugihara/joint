25471c0d73af7ab4313eb7855211b89d180f18e3
Peter Sugihara <peter@campsh.com>
2012-04-23 15:59:10 -0400

    moved pass.js back, found a cleaner way to do npm install

--- bin/pass.js
+++ bin/pass.js
@@ -54,56 +54,10 @@ if (process.argv.length === 5) {
     usageDie('static arg must be directory');
 }
 
+// ####Source Compilation
+
 // if (path.extname(sourcePath) == 'pass')
 //   sourcePath = COMPILED SOURCE
-
-// ###Pass built in functions
-// TODO: Comment the shit out of these.
-
-
-var groups = {};
-
-function contains (arr, obj) {
-  for(var i in arr)
-    if(arr[i] == obj)
-      return true;
-  return false;
-}
-
-function tag (connection, name) {
-  console.log('tagging '+connection.name+' with tag '+name);
-  if(!Boolean(connection.tags))
-    connection.tags = [];
-  if(!contains(connection.tags, name)) {
-    connection.tags.push(name);
-    if(!Boolean(groups[name]))
-      groups[name] = [];
-    groups[name].push(connection);
-    console.log('members of group '+name+':');
-    for(var i in groups[name])
-      console.log('  '+groups[name][i].name);
-  }
-}
-
-function untag (connection, tag) {
-  console.log('untagging '+connection.name+' with tag '+tag);
-  connection.tags.splice(connection.tags.indexOf(tag), 1);
-  groups[tag].splice(groups[tag].indexOf(connection), 1);
-  console.log('members of group '+tag+':');
-  for(var i in groups[tag])
-    console.log('  '+groups[tag][i].name);
-}
-
-function tags (connection) {
-  return connection.tags;
-}
-
-function conns (tag) {
-  console.log('members of group '+tag+':');
-  for(var i in groups[tag])
-    console.log('  '+groups[tag][i].name);
-  return groups[tag];
-}
 
 // ####Server Configuration
 
@@ -111,6 +65,9 @@ var connect = require('connect');
 var browserify = require('browserify');
 var dnode = require('dnode');
 var http = require('http');
+var passProgram = require(sourcePath);
+var lib  = path.join(path.dirname(fs.realpathSync(__filename)), './lib');
+var stdlib = require(lib + '/stdlib.js');
 
 var app = connect();
 
@@ -126,61 +83,19 @@ app.use(connect.static(staticPath));
 
 var server = http.createServer(app);
 
-var passProgram = require(sourcePath);
 
 dnode(function (client, conn) {
-
-  // Expose all functions on the server object.
   var key;
+
+  // Expose all functions on the server object in the passProgram.
   for (key in passProgram.server)
     if (typeof(passProgram.server[key]) === 'function')
       this[key] = passProgram.server[key];
 
-  // this.register = function (callbacks) {
-  //   console.log('CONN ID: '+conn.id);
-  //   for(var i in callbacks) {
-  //     conn[i] = callbacks[i];
-  //   }
-  // };
-
-  // this.setName = function (name) {
-  //   conn.name = name;
-  // };
-
-  // this.join = function (room) {
-  //   var rooms = tags(conn);
-  //   var r, c;
-  //   for(r in rooms)
-  //     if(r == room) return;
-  //   for(r in rooms) {
-  //     var connections = conns(r);
-  //     for(c in connections) {
-  //       if(conn != connections[c])
-  //         connections[c].onLeave(conn.name, r);
-  //     }
-  //     untag(conn, r);
-  //   }
-  //   tag(conn, room);
-  //   var connections = conns(room);
-  //   for(c in connections)
-  //     connections[c].onEnter(conn.name, room);
-  // };
-
-  // this.chat = function (message) {
-  //   var name = conn.name;
-  //   var rooms = tags(conn);
-  //   console.log('tags for '+name+': '+rooms);
-  //   for(var i in rooms) {
-  //     var connections = conns(rooms[i]);
-  //     for(var c in connections)
-  //       connections[c].receive(name, message);
-  //   }
-  // };
-
-  // this.log = function (message) {
-  //   console.log(message);
-  // };
-  
+  // Expose all functions in the stdlib.
+  for (key in passProgram.server)
+    if (typeof(passProgram.server[key]) === 'function')
+      this[key] = passProgram.server[key];
 }).listen(server);
 
 server.listen(port);
