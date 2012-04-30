627aefd35758b35c2b66767db6ea746e7a3ed30c
Rafael Castellanos <rc2633@columbia.edu>
2012-04-30 19:09:16 -0400

    Multiple passes for for code output implmented the first pass is error checking and optimizations The second pass generates the code from the newly optimized AST
    passrafi

--- compiler/PassC.java
+++ compiler/PassC.java
@@ -6,7 +6,7 @@ import java.io.*;
 public class PassC {
     public static PassParser grammar;
     CodeGenerator gen;
-
+    CodeOptimizer opt;
     public void run(CharStream inputStream) throws Exception {
         PassLexer lex = new PassLexer(inputStream);
         TokenRewriteStream tokens = new TokenRewriteStream(lex);
@@ -20,17 +20,27 @@ public class PassC {
             System.exit(-1);
         }
         gen = new CodeGenerator();
-        tree = (PassNode) tree;
-        walkTree(tree);
+        opt = new CodeOptimizer();
+    	try{
+    		optimizeTree(tree);
+        	walkTree(tree);
+        //tree building error
+        }catch(java.lang.ClassCastException e){
+        	System.out.println("ERROR: unable to parse input program");
+		System.exit(-1);
+        }
+
         System.out.println(tree.getText());
     }
 
-    public void walkTree(PassNode n) {
-        Stack<PassNode> s = new Stack<PassNode>();
+   
+   //first pass
+    public void optimizeTree(PassNode n) throws java.lang.ClassCastException{
+    	Stack<PassNode> s = new Stack<PassNode>();
         s.push(n);
         PassNode tmp = null;
-        PassNode leftMostUnvisited = null;
         while (!s.empty()) {
+
             tmp = s.peek();
             for (int i = 0; i < tmp.getChildCount(); i++) {
                 PassNode thisChild = (PassNode) tmp.getChild(i);
@@ -42,9 +52,30 @@ public class PassC {
             if (tmp == s.peek()) {
                 PassNode w = s.pop();
                 w.setVisitedTrue();
+                opt.nodeDecider(w);
+            }
+        }
+    }    
+    //second pass
+    public void walkTree(PassNode n) {
+        Stack<PassNode> s = new Stack<PassNode>();
+        s.push(n);
+        PassNode tmp = null;
+        while (!s.empty()) {
+            tmp = s.peek();
+            for (int i = 0; i < tmp.getChildCount(); i++) {
+                PassNode thisChild = (PassNode) tmp.getChild(i);
+                if (thisChild.isVisited()) {
+                    s.push(thisChild);
+                    break;
+                }
+            }
+            if (tmp == s.peek()) {
+                PassNode w = s.pop();
+                w.setVisitedFalse();
                 String decided = gen.nodeDecider(w);
-                // System.out.println(w.text + " -> " + decided);
                 w.setText(decided);
+                System.out.println(w.getText());
             }
         }
     }
