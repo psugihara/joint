64af62572be1c61481f21654d1d84b577feeccee
Andy Lamping <andrewlamping@gmail.com>
2012-05-06 11:42:45 -0400

    updates

--- Tests/Pass_Tests/Eff_Tests/Efficiency.java
+++ Tests/Pass_Tests/Eff_Tests/Efficiency.java
@@ -1,41 +1,47 @@
+
+/**
+ * -----------------------------------------------------------------------------
+ * author: Andrew Lamping
+ * Efficiency.java
+ * This file prints the elapsed time of a Pass program.
+ *------------------------------------------------------------------------------
+ */
+
 import java.io.IOException;
 
 
+    public class Efficiency {
 
-
-public class Efficiency {
-
-	  public static void main( String[] args ) throws IOException, InterruptedException
-	    {
-		  Runtime rt = Runtime.getRuntime();
-	
+        public static void main( String[] args ) throws IOException, InterruptedException
+	 {
+	     Runtime rt = Runtime.getRuntime();
+	     
+            System.out.println("--------------");
+            System.out.println("Testing " + args[0]);
 		  
-		
-		  System.out.println("--------------");
-		  System.out.println("Testing " + args[0]);
+            // get time before running pass program
+            long before = System.currentTimeMillis();
+            // run pass program in new process
+            Process p = Runtime.getRuntime().exec("java PassC " + args[0]);
 		  
-		  // get time before running pass program
-		  long before = System.currentTimeMillis();
-		  // run pass program in new process
-		  Process p = Runtime.getRuntime().exec("java PassC " + args[0]);
+            // wait for this process to terminate
+            p.waitFor();
 		  
-		  // wait for this process to terminate
-		  p.waitFor();
+            // get time after running pass program/process
+            long after = System.currentTimeMillis();
 		  
-		  // get time after running pass program/process
-		  long after = System.currentTimeMillis();
+            // get elapsed time
+            long elapsedTime = (after-before);
 		  
-		  // get elapsed time
-		  long elapsedTime = (after-before);
-		  
-		  // print elapsed time
-		  System.out.println("Elapsed time in ms = " + elapsedTime);
-		  if(elapsedTime > 2000)
-			  System.out.println("FAILED! FAILED! FAILED!. OVER 2 SEC RUNTIME");
-		  else
-			  System.out.println("PASSED.");
+            // print elapsed time
+            System.out.println("Elapsed time in ms = " + elapsedTime);
+            
+            if(elapsedTime > 2000)
+                System.out.println("FAILED! FAILED! FAILED!. OVER 2 SEC RUNTIME");
+            else
+                System.out.println("PASSED.");
 		 
-		  System.out.println();
+            System.out.println();
 
 	    }
 
