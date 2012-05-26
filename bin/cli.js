0e8b3c33376aca88f3175b312568101e7180d47f
Peter Sugihara <peter@campsh.com>
2012-05-25 19:09:23 -0700

    fixed args

--- bin/cli.js
+++ bin/cli.js
@@ -22,12 +22,12 @@ var sourcePath, port, staticPath, optimize;
 var usageDie = function (message) {
   if (message)
     console.log(message);
-  console.log('usage: spliff <path/program.pass> [<port#> <static directory>][--optimize]');
+  console.log('usage: spliff <path/program.js> <port#> <static directory>');
   process.exit();
 };
 
 // Verify that the command was called with the correct number of arguments.
-if (process.argv.length !== 3 && process.argv.length !== 5 && process.argv.length !== 6) {
+if (process.argv.length !== 5) {
   usageDie('invalid number of arguments');
 }
 
@@ -38,31 +38,19 @@ if (!path.existsSync(sourcePath))
 if (path.extname(sourcePath) !== '.coffee' && path.extname(sourcePath) !== '.js')
   usageDie('Server program must have .coffee or .js extension');
 
-if (process.argv.length > 4 && process.argv.length < 7 ) {
-
   // Verify that the port is a number in the correct range.
   port = parseFloat(process.argv[3]);
   if (!port || port < 0 || port > 65535) {
     usageDie('invalid port number');
   }
 
-  // Verify that the static path exists and points to a directory.
-  staticPath = path.resolve(process.argv[4]);
-  if (!path.existsSync(staticPath))
-    usageDie('file not found: ' + staticPath);
-  if (!fs.statSync(staticPath).isDirectory())
-    usageDie('static arg must be directory');
-  
-  if (process.argv.length === 6) {
-    if (process.argv[5] === '-o' || process.argv[5] === '--optimize') {
-      optimize = true;
-      console.log("optimizing compiled JS...");
-    } else {
-      usageDie('invalid option "' + process.argv[5] + '"');
-    }
-  }
+// Verify that the static path exists and points to a directory.
+staticPath = path.resolve(process.argv[4]);
+if (!path.existsSync(staticPath))
+  usageDie('file not found: ' + staticPath);
+if (!fs.statSync(staticPath).isDirectory())
+  usageDie('static arg must be directory');
 
-}
 
 run(sourcePath);
 
